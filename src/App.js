fc6a7cfb1d51562bcd2c909b6fc7ca67873e9e94
Ilya <shining1234@yandex.ru>
2022-11-17 21:29:48 +0300

    Data transmission via props and improved date output

--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import CostItem from "./components/CostItem";
 
 function App() {
 
-  const costDate = new Date(2021, 2, 12);
-  const costDescription = 'Холодильник';
-  const costAmount = 250;
+  const costs = [
+    {
+      date: new Date(2022, 2, 12),
+      description: 'Холодильник',
+      amount: 450,
+    },
+    {
+      date: new Date(2022, 5, 25),
+      description: 'Ноутбук',
+      amount: 111450,
+    },
+    {
+      date: new Date(2022, 10, 15),
+      description: 'Ноутбук',
+      amount: 111450,
+    },
+  ]
 
   return (
     <div>
       <h1>Учет расходов</h1>
-      <CostItem></CostItem>
+      <CostItem 
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem 
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem 
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,17 +1,21 @@
 import './CostItem.css'
 
-function CostItem() {
+function CostItem(props) {
 
-    const costDate = new Date(2021, 2, 12);
-    const costDescription = 'Холодильник';
-    const costAmount = 450;
+    const month = props.date.toLocaleString('ru-RU', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('ru-RU', {day: '2-digit'});
 
     return (
         <div className="cost-item">
-            <div>{costDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="cost-item__description">
-                <h2>{costDescription}</h2>
-                <div className="cost-item__price">{costAmount} ₽</div>
+                <h2>{props.description}</h2>
+                <div className="cost-item__price">{props.amount} ₽</div>
             </div>
         </div>
     )
