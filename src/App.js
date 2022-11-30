ec33bd29c59f5494494a1a7348f8387a27fdd9ae
Ilya <shining1234@yandex.ru>
2022-11-30 20:11:07 +0300

    transferring elements

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
+import React, {useState} from "react";
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
+const INITIAL_COST = [
+  {
+    id: 'c1',
+    date: new Date(2022, 2, 12),
+    description: 'Холодильник',
+    amount: 450,
+  },
+  {
+    id: 'c2',
+    date: new Date(2022, 5, 25),
+    description: 'Ноутбук',
+    amount: 111450,
+  },
+  {
+    id: 'c3',
+    date: new Date(2022, 10, 15),
+    description: 'Пиджак',
+    amount: 3999,
+  },
+]
+
 const App = () => {
 
-  const costs = [
-    {
-      id: 'c1',
-      date: new Date(2022, 2, 12),
-      description: 'Холодильник',
-      amount: 450,
-    },
-    {
-      id: 'c2',
-      date: new Date(2022, 5, 25),
-      description: 'Ноутбук',
-      amount: 111450,
-    },
-    {
-      id: 'c3',
-      date: new Date(2022, 10, 15),
-      description: 'Пиджак',
-      amount: 3999,
-    },
-  ]
+  const [costs, setCosts] = useState(INITIAL_COST);
 
   const addCostHandler = (cost) => {
-    console.log(cost);
-    console.log('App Component');
+    setCosts(prevCosts => {
+      return [cost, ...prevCosts]
+    });
   }
 
   return (
